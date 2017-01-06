dec6d3bc9146cc40b7577a49585522f7eada856b
ARRS\rkaur <xkaurx@gmail.com>
2017-01-06 10:58:36 -0500

    models created

--- website/music/models.py
+++ website/music/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+#album is a foriegn key in song
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
+
+
+# after you create the classes , go to the terminal inside website and run - python manage.py makemigrations music
+# then run - python manage.py migrate : this will run the migrations and will create tables in the database
